5409ca0b1135cbc13e5761ed47c53b827dc07ae1
kenaimad <dosieniecki@gmail.com>
2022-03-29 18:42:17 +0200

    Add form to send emails from ContactPage.

--- landing-page/src/pages/ContactPage/ContactForm/ContactForm.js
+++ landing-page/src/pages/ContactPage/ContactForm/ContactForm.js
@@ -1,24 +1,69 @@
+import {StyledForm, StyledButton, StyledItem, StyledInput, StyledFormContainer, StyledTitle} from "./ContactForm.styled";
+
+const validateMessages = {
+  required: '${label} jest wymagany!',
+  types: {
+    email: '${label} nie jest prawidłowym adresem mailowym!',
+  },
+};
+/* eslint-enable no-template-curly-in-string */
 
 const ContactForm = () => {
+  const onFinish = (values) => {
+    fetch('https://formsubmit.co/ajax/dosieniecki@gmail.com', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({
+        name: values.user.email,
+        message: values.user.message,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error));
+  };
 
   return (
-    <form action="https://formsubmit.co/dosieniecki@gmail.com" method="POST">
-    <div>
-      <div>
-        <div>
-          <input type="text" name="name"  placeholder="Imię" required />
-        </div>
-        <div>
-          <input type="email" name="email" placeholder="Email" required />
-        </div>
-        
-      </div>
-    </div>
-    <div class="form-group">
-      <textarea placeholder="Twoja wiadomość"name="message" rows="10" required></textarea>
-    </div>
-    <button type="submit">Submit Form</button>
-  </form>
+    <StyledFormContainer>
+      <StyledTitle>Masz jakieś pytanie?</StyledTitle>
+    <StyledForm
+      name="nest-messages"
+      onFinish={onFinish}
+      validateMessages={validateMessages}
+    >
+      <StyledItem
+        name={['user', 'email']}
+        label="Email"
+        rules={[
+          {
+            required: true,
+            type: 'email',
+          },
+        ]}
+      >
+        <StyledInput />
+      </StyledItem>
+      <StyledItem
+        name={['user', 'message']}
+        label="Wiadomość:"
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+      >
+        <StyledInput.TextArea showCount maxLength={300}/>
+      </StyledItem>
+      <StyledItem>
+        <StyledButton type="primary" htmlType="submit">
+          Wyślij
+        </StyledButton>
+      </StyledItem>
+    </StyledForm>
+    </StyledFormContainer>
   );
 };
 
--- landing-page/src/pages/ContactPage/ContactForm/ContactForm.styled.js
+++ landing-page/src/pages/ContactPage/ContactForm/ContactForm.styled.js
@@ -2,8 +2,47 @@ import styled from 'styled-components';
 
 import { Form, Input, Button } from 'antd';
 import 'antd/dist/antd.min.css';
+export const StyledFormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+`;
 
-export const StyledForm = styled(Form)``
-export const StyledInput = styled(Input)``
-export const StyledButton = styled(Button)``
-export const StyledItem = styled(Form.Item)``
+export const StyledTitle = styled.h1`
+  font-family: 'Poppins', sans-serif;
+  color: #c4ab9d;
+  font-weight: bolder;
+  margin: 2rem;
+`;
+export const StyledForm = styled(Form)``;
+export const StyledInput = styled(Input)`
+  border: 1px solid #c4ab9d;
+
+  &.active {
+      background-color: red;
+      color: black;
+    }
+`;
+export const StyledButton = styled(Button)`
+  background-color: white;
+  color: #c4ab9d;
+  border: 1px solid #c4ab9d;
+  transition: all 0.1s ease-in;
+  font-family: 'Poppins', sans-serif;
+  font-weight: bolder;
+
+  &:hover {
+    background-color: #c4ab9d;
+    color: white;
+    border: none;
+  }
+`;
+
+export const StyledItem = styled(Form.Item)`
+  label {
+    width: 120px;
+    font-family: 'Poppins', sans-serif;
+    color: #c4ab9d;
+    font-weight: bolder;
+  }
+`;
