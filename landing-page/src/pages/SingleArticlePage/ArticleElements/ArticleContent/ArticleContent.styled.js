5fd4b3c637cbb40b2edacc4d046f7e2e03291eaf
kenaimad <dosieniecki@gmail.com>
2022-07-30 17:39:07 +0200

    Remove fontsize querries from SingleArticlePage.

--- landing-page/src/pages/SingleArticlePage/ArticleElements/ArticleContent/ArticleContent.styled.js
+++ landing-page/src/pages/SingleArticlePage/ArticleElements/ArticleContent/ArticleContent.styled.js
@@ -16,57 +16,12 @@ export const Title = styled(StyledSubtitle)`
   text-align: left;
   font-size: 1.6rem;
 
-  @media (max-width: 2000px) {
-    font-size: 1.7rem;
-  }
-
-  @media (max-width: 1600px) {
-    font-size: 1.6rem;
-  }
-
-  @media (max-width: 1200px) {
-    font-size: 1.5rem;
-  }
-
-  @media (max-width: 1200px) {
-    font-size: 1.3rem;
-  }
-
-  @media (max-width: 800px) {
-    font-size: 1.4rem;
-  }
-
-  @media (max-width: 600px) {
-    font-size: 1.2rem;
-  }
 `;
 
 export const Paragraph = styled(StyledParagraph)`
   text-align: left;
   font-size: 1.4rem;
-  @media (max-width: 2000px) {
-    font-size: 1.3rem;
-  }
 
-  @media (max-width: 1600px) {
-    font-size: 1.2rem;
-  }
-
-  @media (max-width: 1200px) {
-    font-size: 1.1rem;
-  }
-
-  @media (max-width: 1200px) {
-    font-size: 0.9rem;
-  }
-
-  @media (max-width: 800px) {
-    font-size: 1.1rem;
-  }
-
-  @media (max-width: 600px) {
-    font-size: .9rem;
-  }
 `;
 
 
