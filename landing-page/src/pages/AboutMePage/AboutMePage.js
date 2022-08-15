fe4ba8cfa9adf715c2ad947fa7f038563cfa5a8b
kenaimad <dosieniecki@gmail.com>
2022-08-15 19:50:10 +0200

    Remove function declaration in AboutMePage.

--- landing-page/src/pages/AboutMePage/AboutMePage.js
+++ landing-page/src/pages/AboutMePage/AboutMePage.js
@@ -22,24 +22,9 @@ const AboutMePage = () => {
     },
   ];
 
-  // const writePageContent = () => {
-  //   return pageContent.map((paragraph) => {
-  //     return (
-  //       <PageSection
-  //         key={paragraph.text}
-  //         title={paragraph.title}
-  //         text={paragraph.text}
-  //         img={paragraph.img}
-  //         endingText={paragraph.endingText}
-  //       />
-  //     );
-  //   });
-  // };
-
   return (
     <PageWrapper>
       <MobileTitle>Cześć!</MobileTitle>
-      {/* {writePageContent()} */}
       {pageContent.map((paragraph) => {
         return (
           <PageSection
