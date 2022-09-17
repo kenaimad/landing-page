d4015fe61836c17c66aff2e73b9257ebeac04548
kenaimad <dosieniecki@gmail.com>
2022-09-17 20:35:56 +0200

    Diseases images repaired.

--- landing-page/src/pages/OfferPage/Sections/ImagesSection/ImagesSection.styled.js
+++ landing-page/src/pages/OfferPage/Sections/ImagesSection/ImagesSection.styled.js
@@ -6,12 +6,16 @@ export const ImagesSection = styled.section`
   display: flex;
   justify-content: center;
   align-items: center;
-  flex-wrap: wrap;
+  flex-wrap: nowrap;
+
+  @media (max-width: 600px) {
+      flex-wrap: wrap;
+    }
 
   img {
     object-fit: contain;
-    width: 20%;
-    min-width: 100px;
+    width: 25%;
+    //min-width: 100px;
     flex-grow: 0;
     padding-left: 1rem;
     padding-top: 0.5rem;
@@ -19,6 +23,10 @@ export const ImagesSection = styled.section`
 
     &:hover {
       transform: scale(1.1);
+    }
+
+    @media (max-width: 600px) {
+      width: 50%;
     }
   }
 `;
